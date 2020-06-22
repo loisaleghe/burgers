b406f34a1de389260ac7defa1a8bda7b10af6e40
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-06-22 14:49:22 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2')
 
 let connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+  connection = mysql
+  .createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Slois123.',
+    database: 'burgers_db'
+  })
+  .promise()
+}
 
-connection = mysql
-.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Slois123.',
-  database: 'burgers_db'
-})
-.promise()
 
 connection.connect()
 module.exports = connection;
